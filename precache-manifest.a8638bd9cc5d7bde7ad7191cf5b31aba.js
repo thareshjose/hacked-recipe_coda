b77083dd7b3c4a8c101a77cee67bdd2f4a88ae6d
Tharesh Jose <99tharesh@gmail.com>
2020-08-10 01:10:52 +0530

    Updates

---
+++ precache-manifest.a8638bd9cc5d7bde7ad7191cf5b31aba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f13a13b4f2d42401a8c09fb61b0a0f0",
+    "url": "/hacked-recipe_coda/index.html"
+  },
+  {
+    "revision": "6d80b90130f179d943cc",
+    "url": "/hacked-recipe_coda/static/css/main.816e41e5.chunk.css"
+  },
+  {
+    "revision": "9b54fd848a3842fdd425",
+    "url": "/hacked-recipe_coda/static/js/2.bc778ead.chunk.js"
+  },
+  {
+    "revision": "a1a636632721a402612df3e043dd630d",
+    "url": "/hacked-recipe_coda/static/js/2.bc778ead.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d80b90130f179d943cc",
+    "url": "/hacked-recipe_coda/static/js/main.952df86b.chunk.js"
+  },
+  {
+    "revision": "c4ffa50d7badfb95b8ab",
+    "url": "/hacked-recipe_coda/static/js/runtime-main.ee0a7459.js"
+  }
+]);
